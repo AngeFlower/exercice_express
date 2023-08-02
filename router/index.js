7858a440a241554b35b7874db099e56c3feb08e7
AngeFlower <angefleurisseineza@gmail.com>
2023-08-02 21:46:32 +0200

    some confugurations

---
+++ router/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const coursController = require('../controller/cours');
+
+// router.post('/cours', coursController.createCours);
+// router.get('/cours/', coursController.retrieveAllCours);
+// router.get('/cours/:id', coursController.retrieveCours);
+// router.patch('/cours/:id', coursController.updateCours);
+// router.delete('/cours/:id', coursController.deleteCours);
+
+
+module.exports = router;
